9780e80c02db04a0f4ec2b7c0d6103a40202f475
Sergey Vasilyev <s2504s@gmail.com>
2017-08-03 10:39:47 +0300

    Fixed whitespaces

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 module "tf_label" {
-  source = "github.com/cloudposse/tf_label"
+  source    = "github.com/cloudposse/tf_label"
   namespace = "${var.namespace}"
-  stage = "${var.stage}"
-  name = "${var.name}"
+  stage     = "${var.stage}"
+  name      = "${var.name}"
 }
 
 resource "aws_ami_from_instance" "create_ami" {
-  name               = "${module.tf_label.id}"
-  source_instance_id = "${var.source_instance_id}"
+  name                    = "${module.tf_label.id}"
+  source_instance_id      = "${var.source_instance_id}"
   snapshot_without_reboot = "true"
-  tags = "${module.tf_label.tags}"
+  tags                    = "${module.tf_label.tags}"
+
   lifecycle {
     create_before_destroy = true
   }
