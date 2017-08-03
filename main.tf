6a6d17b0b2b42d29818591d59482cde96ae1292d
Sergey Vasilyev <s2504s@gmail.com>
2017-08-03 09:45:30 +0300

    Used tf_label for all names

--- main.tf
+++ main.tf
@@ -5,12 +5,11 @@ module "tf_label" {
   name = "${var.name}"
 }
 
-
 resource "aws_ami_from_instance" "create_ami" {
   name               = "${module.tf_label.id}"
   source_instance_id = "${var.source_instance_id}"
   snapshot_without_reboot = "true"
-  tags = "${var.tags}"
+  tags = "${module.tf_label.tags}"
   lifecycle {
     create_before_destroy = true
   }
